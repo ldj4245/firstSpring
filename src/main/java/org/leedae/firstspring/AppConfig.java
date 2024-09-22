59544c3fba66c79eb706136c4a299501890dd4ba
LEEDAEJIN <ldj4241@naver.com>
2024-09-23 01:15:30 +0900

    스프링으로 변경

--- src/main/java/org/leedae/firstspring/AppConfig.java
+++ src/main/java/org/leedae/firstspring/AppConfig.java
@@ -9,23 +9,32 @@ import org.leedae.firstspring.member.MemberServiceImpl;
 import org.leedae.firstspring.member.MemoryMemberRepository;
 import org.leedae.firstspring.order.OrderService;
 import org.leedae.firstspring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
--- src/main/java/org/leedae/firstspring/MemberApp.java
+++ src/main/java/org/leedae/firstspring/MemberApp.java
@@ -4,13 +4,18 @@ import org.leedae.firstspring.member.Grade;
 import org.leedae.firstspring.member.Member;
 import org.leedae.firstspring.member.MemberService;
 import org.leedae.firstspring.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
