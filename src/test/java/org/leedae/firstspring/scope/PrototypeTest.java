178fabd7444e83a4dbe83240500bbd0f51603ba2
LEEDAEJIN <ldj4241@naver.com>
2024-09-24 22:35:26 +0900

    bean scope 적용해보기

---
+++ src/test/java/org/leedae/firstspring/scope/PrototypeTest.java
@@ -0,0 +1,50 @@
+package org.leedae.firstspring.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("findtypeBean1");
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("findTypebean2");
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroyed();
+        prototypeBean2.destroyed();
+
+        ac.close();
+
+
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroyed(){
+            System.out.println("PrototypeBean.destroyed");
+        }
+
+    }
+}
--- src/test/java/org/leedae/firstspring/singleton/ConfigurationSingleTonTest.java
+++ src/test/java/org/leedae/firstspring/singleton/ConfigurationSingleTonTest.java
@@ -3,7 +3,9 @@ package org.leedae.firstspring.singleton;
 import org.junit.jupiter.api.Test;
 import org.leedae.firstspring.AppConfig;
 import org.leedae.firstspring.member.MemberRepository;
+import org.leedae.firstspring.member.MemberService;
 import org.leedae.firstspring.member.MemberServiceImpl;
+import org.leedae.firstspring.order.OrderService;
 import org.leedae.firstspring.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -18,16 +20,29 @@ public class ConfigurationSingleTonTest {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
 
+
+        MemberService memberService4 = ac.getBean("memberService",MemberService.class);
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        MemberService memberService1 = ac.getBean(MemberServiceImpl.class);
+        OrderService orderService1 = ac.getBean(OrderService.class);
+        OrderService orderService2 = ac.getBean("orderService",OrderServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService",OrderServiceImpl.class);
         MemberRepository memberRepository = ac.getBean("memberRepository",MemberRepository.class);
 
         MemberRepository memberRepository1 = memberService.getMemberRepository();
         MemberRepository memberRepository2 = orderService.getMemberRepository();
 
+
+        System.out.println("orderService = " + orderService);
+        System.out.println("orderService1 = " + orderService1);
+        System.out.println("orderService2 = " + orderService2);
+
         System.out.println("memberRepository1 = " + memberRepository1);
         System.out.println("memberRepository2 = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService1 = " + memberService1);
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
