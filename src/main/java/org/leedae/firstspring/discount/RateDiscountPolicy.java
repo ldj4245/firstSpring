569d54d708263d6ac87a9f2f493e6e65e909a8d4
LEEDAEJIN <ldj4241@naver.com>
2024-09-24 17:09:42 +0900

    전략 패턴 적용해보기

--- src/main/java/org/leedae/firstspring/discount/RateDiscountPolicy.java
+++ src/main/java/org/leedae/firstspring/discount/RateDiscountPolicy.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy  {
 
     private int discountPercent = 10 ;
--- src/main/java/org/leedae/firstspring/order/OrderServiceImpl.java
+++ src/main/java/org/leedae/firstspring/order/OrderServiceImpl.java
@@ -10,14 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
 
-    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
